3228ab0f056aef244334e13e149045068a565488
Alexander Shander <sylvl0n@yandex.ru>
2023-07-17 12:02:13 +0300

    14

---
+++ easy_14_longest_common_prefix/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+import "strings"
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0 {
+        return ""
+    }
+
+    pref := ""
+    l := 1
+
+    for {
+        if len(strs[0]) < l {
+            return pref
+        }
+
+        p := strs[0][0:l]
+
+        for _, s := range strs {
+            if len(s) < l {
+                return pref
+            }
+
+            if p != s[0:l] {
+                return pref
+            }
+        }   
+        l++
+        pref = p
+    }
+    
+
+    return pref
+}
